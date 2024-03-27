1fd1c25e7fdf308b7cdd9b26a6ccf8ad67289b7b
Ritik Gupta <ritikguptaa1942@gmail.com>
2024-03-27 22:54:41 +0530

    Commit 0.1.8 Noc Process Complete

--- app/src/main/java/com/example/blockchain/AdminNocApprove.java
+++ app/src/main/java/com/example/blockchain/AdminNocApprove.java
@@ -1,5 +1,6 @@
 package com.example.blockchain;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,8 +12,15 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class AdminNocApprove extends AppCompatActivity {
 
@@ -20,6 +28,10 @@ public class AdminNocApprove extends AppCompatActivity {
     RecyclerView adminNocRv;
     FirebaseRecyclerOptions<AdminNocModel> options;
     AdminNocAdapter nocAdapter;
+    String userEmail,convertedEmail;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    DatabaseReference userTypeRef = FirebaseDatabase.getInstance().getReference();
+    Query query;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +42,45 @@ public class AdminNocApprove extends AppCompatActivity {
 
         WrapContentLinearLayoutManager nocAdminLayout = new WrapContentLinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false);
         adminNocRv.setLayoutManager(nocAdminLayout);
-        Query query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(0);
 
-        options = new FirebaseRecyclerOptions.Builder<AdminNocModel>()
-                .setQuery(query, AdminNocModel.class)
-                .build();
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            userEmail = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
+        }
+        assert userEmail != null;
+        convertedEmail = userEmail.replaceAll("\\.", "%7");
 
-        nocAdapter = new AdminNocAdapter(options);
-        adminNocRv.setAdapter(nocAdapter);
-        nocAdapter.startListening();
+        userTypeRef.child("user").child(convertedEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    int deptType = Integer.parseInt(snapshot.child("dept").getValue().toString());
+                    if(deptType == 2){
+                        query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(0);
+                    } else if(deptType == 3){
+                        query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(1);
+                    } else if(deptType == 4){
+                        query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(2);
+                    } else if(deptType == 5){
+                        query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(3);
+                    } else if(deptType == 6){
+                        query = FirebaseDatabase.getInstance().getReference().child("noc").orderByChild("Status").equalTo(4);
+                    }
+                    options = new FirebaseRecyclerOptions.Builder<AdminNocModel>()
+                            .setQuery(query, AdminNocModel.class)
+                            .build();
+
+                    nocAdapter = new AdminNocAdapter(options);
+                    adminNocRv.setAdapter(nocAdapter);
+                    nocAdapter.startListening();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         backBtn.setOnClickListener( v -> finish());
     }
@@ -46,6 +88,12 @@ public class AdminNocApprove extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        nocAdapter.stopListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
         nocAdapter.stopListening();
     }
 
--- app/src/main/java/com/example/blockchain/AdminNocRemark.java
+++ app/src/main/java/com/example/blockchain/AdminNocRemark.java
@@ -3,11 +3,13 @@ package com.example.blockchain;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,8 +18,20 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class AdminNocRemark extends AppCompatActivity {
 
@@ -28,6 +42,11 @@ public class AdminNocRemark extends AppCompatActivity {
     String studentKey, type, isDelete = "0";
     Uri itemPurl;
     AppCompatButton approveBtn;
+    String adminType,adminName;
+    int Status;
+    String userEmail,convertedEmail;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    DatabaseReference userTypeRef = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +64,48 @@ public class AdminNocRemark extends AppCompatActivity {
 
         studentKey = getIntent().getStringExtra("key");
         type = getIntent().getStringExtra("type");
-        Log.d("oio", "onCreate: "+type+studentKey);
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            userEmail = Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
+        }
+        assert userEmail != null;
+        convertedEmail = userEmail.replaceAll("\\.", "%7");
 
         if(type.equals("1")){
             approveBtn.setText("Reject");
             approveBtn.setBackgroundDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.light_red_btn));
         }
+
+        userTypeRef.child("user").child(convertedEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    int deptType = Integer.parseInt(snapshot.child("dept").getValue().toString());
+                    adminName = snapshot.child("name").getValue().toString();
+                    if(deptType == 2){
+                        adminType = "guide";
+                        Status = 1;
+                    } else if(deptType == 3){
+                        adminType = "projectcod";
+                        Status = 2;
+                    } else if(deptType == 4){
+                        adminType = "placement";
+                        Status = 3;
+                    } else if(deptType == 5){
+                        adminType = "finance";
+                        Status = 4;
+                    } else if(deptType == 6){
+                        adminType = "admin";
+                        Status = 5;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
                 registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
@@ -104,6 +159,14 @@ public class AdminNocRemark extends AppCompatActivity {
 
         });
 
+        approveBtn.setOnClickListener( v ->{
+            if(type.equals("1")){
+                uploadRemark(1,adminType,itemPurl,remarkEdt.getText().toString(),adminName);
+            } else if(type.equals("0")){
+                uploadRemark(0,adminType,itemPurl,remarkEdt.getText().toString(),adminName);
+            }
+        });
+
     }
 
     private String getFileNameFromUri(Uri uri) {
@@ -116,7 +179,41 @@ public class AdminNocRemark extends AppCompatActivity {
         }
         return fileName;
     }
-    private void uploadRemark(String type, Uri imgUri, String remark){
+    private void uploadRemark(int appRej,String type, Uri imgUri, String remark, String name){
+        DatabaseReference nocRef = FirebaseDatabase.getInstance().getReference("noc").child(studentKey);
+        HashMap<String,Object> remarkMap = new HashMap<>();
+        HashMap<String,Object> nocStatusMap = new HashMap<>();
+        if(appRej == 0){
+            nocStatusMap.put(type,2);
+            nocStatusMap.put("Status",Status);
+        }
+        else nocStatusMap.put(type,0);
 
+        if(imgUri == null){
+            remarkMap.put("name",name);
+            remarkMap.put("remark",remark);
+            nocRef.updateChildren(nocStatusMap).addOnSuccessListener(s ->{
+                nocRef.child("Remark").child(type).updateChildren(remarkMap);
+                Toast.makeText(this, "NOC Rejected", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this,AdminDashboard.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            });
+        }
+        else {
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference("NocRemark/" + studentKey );
+            storageRef.putFile(imgUri).addOnCompleteListener(task -> {
+               if(task.isSuccessful()){
+                   storageRef.getDownloadUrl().addOnSuccessListener(uri ->{
+                       remarkMap.put("name",name);
+                       remarkMap.put("remark",remark);
+                       remarkMap.put("purl",uri.toString());
+                       nocRef.updateChildren(nocStatusMap).addOnSuccessListener( s->{
+                           nocRef.child("Remark").child(type).updateChildren(remarkMap);
+                           Toast.makeText(this, "NOC Rejected", Toast.LENGTH_SHORT).show();
+                           startActivity(new Intent(this,AdminDashboard.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                       });
+                   });
+               }
+            });
+        }
     }
 }
